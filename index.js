406579b2bca357a2b5174c02c06228582db88e3e
ninaraposo1986 <ninaraposo1986@gmail.com>
2020-04-16 16:46:12 -0500

    used temp and unit as parameters for temperatureInF, made an if statement that said if unit was equal to 'F', to return the temp + unit parameters and otherwise return temperatureCtF with parameter of temp + 'F'.

--- index.js
+++ index.js
@@ -75,8 +75,12 @@ function temperatureCtoF(temp) {
  * 
  * Hint: You can call your `temperatureCtoF` function from inside `temperatureInF`.
 */
-function temperatureInF(/* code here */) {
-  /* code here */
+function temperatureInF(temp, unit) {
+  if (unit === 'F') {
+    return `${temp}` + unit;
+  } else {
+    return `${temperatureCtoF(temp)}` + 'F'
+  }
 }
 
 
